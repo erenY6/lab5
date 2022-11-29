424d344d4da24c01f632b7b88c653eea1758315a
erenY6 <104152675+erenY6@users.noreply.github.com>
2022-11-29 15:20:06 +0300

    Add files via upload

--- load.c
+++ load.c
@@ -1,22 +1,22 @@
 #include "load.h"
 #include "fun.h"
 #include <stdio.h>
-#include <windows.h>
+#include <dlfcn.h>
 void LoadRun(const char * const s) {
 void * lib;
 void (*fun)(void);
 void (*fun1)(void);
-lib = LoadLibrary(s);
+lib = dlopen(s, RTLD_LAZY);
 if (!lib) {
 printf("cannot open library '%s'\n", s);
 return;
 }
-fun = (void (*)(void))GetProcAddress((HINSTANCE)lib, "obrMas");
+fun = (void (*)(void))dlsym(lib, "obrMas");
 if (fun != NULL)
 fun();
-fun1 = (void (*)(void))GetProcAddress((HINSTANCE)lib, "obrMat");
+fun1 = (void (*)(void))dlsym(lib, "obrMat");
 if (fun1 != NULL)
 fun1();
-FreeLibrary((HINSTANCE)lib);
+dlcloce(lib);
 }
 
